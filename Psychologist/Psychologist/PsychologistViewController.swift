740d0a09bc17f6e199feac7d74201ed1a0e96a21
joanne <jojoannexo@gmail.com>
2015-06-29 20:30:13 +0530

    multiple MVCs -- core build. next step: implement segues

---
+++ Psychologist/Psychologist/PsychologistViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Psychologist
+//
+//  Created by Joanne Koong on 6/29/15.
+//  Copyright (c) 2015 joanne. All rights reserved.
+//
+
+import UIKit
+
+class PsychologistViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
